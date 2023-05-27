34cead44280297bd69b5abacf8ec7a7dfe27a851
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-05-27 21:38:38 +0300

    Mobile Menu

--- src/components/common/header/MobileMenu.jsx
+++ src/components/common/header/MobileMenu.jsx
@@ -3,30 +3,24 @@ import MobileMenuContent from "./MobileMenuContent";
 
 const MobileMenu = () => {
   return (
-    <div className="stylehome1 h0 mega-menu-wrapper">
+    <div className="stylehome1 h0 mega-menu-wrapper" >
       <div className="mobile-menu">
-        <div className="header stylehome1" style={{backgroundColor: 404041,}}>
+        <div className="header stylehome1" >
           <div className="main_logo_home2 text-center">
             <div className="d-flex align-items-center justify-content-between">
               
               <img
                 className="nav_logo_img img-fluid mt20"
-                src="https://res.cloudinary.com/dhk7qsnfv/image/upload/v1684257816/header-logo2_bawqi1.svg"
+                src="https://res.cloudinary.com/dhk7qsnfv/image/upload/v1685212478/header-logo2_bawqi1_1_-svg_j6fhp4.webp"
                 alt="header-logo2.svg"
-                width={100}
+                width={250}
 
               />
-            <img
-                className="nav_logo_img img-fluid mt20 ml10"
-                style={{marginRight: 50,}}
-                src="https://res.cloudinary.com/dhk7qsnfv/image/upload/v1684257815/winner-land_jm56zk.webp"
-                alt="header-logo2.svg"
-                width={130}
-              />
+           
             </div>
           </div>
 
-          <ul className="menu_bar_home2">
+          <ul className="menu_bar_home2"  style={{color: 'white'}}>
             <li
               className="list-inline-item mt10"
               data-bs-toggle="offcanvas"
@@ -34,7 +28,7 @@ const MobileMenu = () => {
               aria-controls="offcanvasMenu"
             >
               <a>
-                <span></span>
+                <span>القائمة</span>
               </a>
             </li>
           </ul>
