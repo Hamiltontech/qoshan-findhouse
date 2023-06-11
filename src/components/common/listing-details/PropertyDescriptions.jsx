f8dcab15c0a7cac09bbddda7ea022d03b8da00fb
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-06-11 08:54:24 +0000

    youtubelink - area prefix - not on search

--- src/components/common/listing-details/PropertyDescriptions.jsx
+++ src/components/common/listing-details/PropertyDescriptions.jsx
@@ -1,9 +1,23 @@
 import ReactHtmlParser from 'react-html-parser';
+import YouTube from "react-youtube";
 
 const PropertyDescriptions = ({property}) => {
-
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <>
+    {property?.x_studio_youtube_video? 
+    
+    <YouTube videoId={property?.x_studio_youtube_video.slice(17)} 
+    opts={opts} />
+  :
+  <></>
+  }
     {property?.x_studio_property_information && 
      <p>{ ReactHtmlParser(property?.x_studio_property_information) }</p> 
     }
