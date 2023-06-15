76327877454d7ba84723de85b4c8b18982ba8b23
dialaabulkhail <diala.sh.98@gmail.com>
2023-06-15 07:25:05 +0000

    no scroll

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -1,11 +1,15 @@
 import nodemailer from 'nodemailer';
 import multer from 'multer';
+import express from 'express';
+
+
+const app = express();
 
 export default async (req, res) => {
   if (req.method === 'POST') {
-    const { name, email, phone, area, propertyType, sellingPrice, PropertyArea, body } = req.body;
-    const upload = multer({ dest: 'uploads/' });
-
+    const { name, email, phone, area, propertyType, sellingPrice, PropertyArea, body, propertyImages } = req.body;
+    const upload = multer({ storage: multer.memoryStorage() });
+ 
 const transporter = nodemailer.createTransport({
     host: 'mail.esmrton.com',
     port: 465,
@@ -15,21 +19,20 @@ const transporter = nodemailer.createTransport({
       pass: 'Qoshan@2020',
     },
   });
-  
+
 
     try {
-      // Send email
-      await transporter.sendMail({
-        from: email,
-        to: 'info@qoshan.com',
-        subject: 'آعلن عن عقارك',
-        text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nArea: ${area}\nProperty Type: ${propertyType}\nSelling Price: ${sellingPrice} \nProperty Area: ${PropertyArea} \n${body}`,
-       attachments : [
-        {
-          filename: "testing.png",
-          path: "https://res.cloudinary.com/dhk7qsnfv/image/upload/v1686130405/1-7_glnwhu_x8w7rg.webp"
-        }
-       ]
+      app.post('/upload', upload.single('propertyImages'), (req, res) => {
+        transporter.sendMail({
+          from: "incoming@qoshan.com",
+          to: "info@qoshan.com",
+          subject: 'آعلن عن عقارك',
+          html: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nArea: ${area}\nProperty Type: ${propertyType}\nSelling Price: ${sellingPrice} \nProperty Area: ${PropertyArea} \n${body}`,
+          attachments: [{
+            filename: req.file.propertyImages,
+            content: req.file.buffer
+          }]
+        })
       });
 
       res.status(200).json({ message: 'تم ارسال الرسالة بنجاح' });
